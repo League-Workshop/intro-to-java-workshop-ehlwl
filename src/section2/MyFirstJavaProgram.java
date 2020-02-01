97ca3df64173f52ea199ef85413d94e913e0d983
League Student <league@iMac-23.attlocal.net>
2020-02-01 15:55:18 -0800

    section 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,28 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
 		
-		
+		Robot rob = new Robot("mini");
+		Robot ro = new Robot("mini");
+		rob.setSpeed(100);
+		rob.penDown();
+		rob.move(100);
+		rob.turn(180);
+		rob.move(50);
+		rob.turn(270);
+		rob.move(50);
+		rob.turn(90);
+		rob.move(50);
+		rob.turn(180);
+		rob.move(100);
+		rob.hide();
+	    ro.setSpeed(100);
+		ro.moveTo(200, 250);
+		ro.penDown();
+		for(int i = 0; i < 999; i++){
+			ro.setRandomPenColor();
+			ro.move(100);
+			ro.turn(90);
+		}
 	}
 }
--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -6,19 +6,24 @@ public class SmartShapes {
 	
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
+    	Robot rob = new Robot("mini");
 
-
+    	rob.moveTo(20, 500);
         // 3. Put the robot's pen down
-
+    	rob.penDown();
 
         // 6. Make the robot move as fast as possible
-
+    	rob.setSpeed(100);
 
         // 5. Use a for loop to repeat everything below 4 times. 
-
-
+    	for(int i = 0; i < 360;i++) {
+    		rob.setRandomPenColor();
+    		rob.move(5);
+    		rob.turn(1);
+    	}
+    	rob.hide();
         //         2. Move your robot 200 pixels
-
+    	
 
         //         4. Turn the robot 90 degrees to the right
 
